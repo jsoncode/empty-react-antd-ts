d0f0ed652da1467b7336312a4ea1ae7a689ab0f5
jsoncode <jsoncode@example.com>
2022-06-21 18:01:04 +0800

    feat: 启用less module

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Button } from 'antd';
+import styles from './App.module.less'
 
 function App() {
     return (
-        <div>
+        <div className={styles.page}>
             <Button>button</Button>
         </div>
     );
