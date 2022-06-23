fa562068bbc13b7efaebce8646a60b99d69a5752
jsoncode <jsoncode@example.com>
2022-06-23 15:55:23 +0800

    feat: 添加svg等图片的引用支持

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,9 +1,13 @@
 import React, { FC } from 'react';
 import { Link } from "react-router-dom";
+import Icon from '@ant-design/icons'
 import styles from '@/pages/App.module.less'
+
+import { ReactComponent as Test } from '@/assets/test.svg'
 
 const Index: FC = () => {
     return <div className={styles.appPage}>
+        <Icon component={Test}/>
         <Link to={'/user'}>aaaa</Link>
         <Link to={'/admin'}>bbbb</Link>
     </div>
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -4,3 +4,12 @@ declare module '*.module.less' {
     }
     export default classes
 }
+
+//在typscript中无法识别非代码资源, 需要主动的去声明这些module
+declare module '*.svg'
+declare module '*.png'
+declare module '*.jpg'
+declare module '*.jpeg'
+declare module '*.gif'
+declare module '*.bmp'
+declare module '*.tiff'
