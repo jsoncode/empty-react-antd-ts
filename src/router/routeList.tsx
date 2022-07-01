ab8cc9685d4044a8f0022c4c26dee6246fd017cb
jsoncode <jsoncode@example.com>
2022-07-01 14:13:40 +0800

    feat: 扁平化路由

--- src/router/routeList.tsx
+++ src/router/routeList.tsx
@@ -3,23 +3,50 @@ import { RouteObject } from 'react-router-dom';
 import NotFound from '@/pages/NotFound';
 import PageContainer from "@/pages";
 import Home from "@/pages/Home";
-import { customComponents } from "@/router/customComponents";
-import { utilsComponents } from "@/router/utilsComponents";
+import Drawer from "@/components/Darwer";
+import ChineseInput from "@/pages/SubPages/ChineseInput";
+import EasyOut from "@/pages/SubPages/EasyOut";
+import Ajax from "@/pages/SubPages/Ajax";
+import IndexDB from "@/pages/SubPages/IndexDB";
+import DoubleCharCodeLength from "@/pages/SubPages/DoubleCharCodeLength";
 
 // 主路由
 export const routeList: RouteObject[] = [
     {
         // 这里的pageContainer只是一个壳子,包含了公共的部分内容,子路由中的index才算真正首页
-        path: '/', element: <PageContainer/>,
+        path: '*', element: <PageContainer/>,
         children: [
             {
                 index: true, element: <Home/>,
             },
-            ...customComponents,
-            ...utilsComponents,
+            {
+                path: 'Drawer',
+                element: <Drawer/>
+            },
+            {
+                path: 'ChineseInput',
+                element: <ChineseInput/>
+            },
+
+            {
+                path: 'EasyOut',
+                element: <EasyOut/>
+            },
+            {
+                path: 'Ajax',
+                element: <Ajax/>
+            },
+            {
+                path: 'IndexDB',
+                element: <IndexDB/>
+            },
+            {
+                path: 'DoubleCharCodeLength',
+                element: <DoubleCharCodeLength/>
+            },
+            {
+                path: '*', element: <NotFound/>,
+            },
         ]
-    },
-    {
-        path: '*', element: <NotFound/>,
     },
 ];
